9eed145be25d5e1e22c46ec30af8a382661527b8
Jose Donizetti <jdbjunior@gmail.com>
2012-04-10 23:02:46 -0300

    projeto para cadastrar ideias para o fedexday

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Projetos::Application.config.secret_token = '59b64018a7b2c57d43e706756c9655389ac4b8140087098f4e3c0e8782991d175c9fe58e15278e33cb98e58276a9bb82fcf67eb7fefdb779fed5275d8ab40a78'
